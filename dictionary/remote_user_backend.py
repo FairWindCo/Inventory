bc9ccc8df0b2cda6b234b56595e6bcfdb9288fd2
FairWind <sergey.manenok@gmail.com>
2022-05-23 15:17:06 +0300

    add support for remote user auth

--- dictionary/remote_user_backend.py
+++ dictionary/remote_user_backend.py
@@ -1,11 +1,18 @@
+from django.conf import settings
 from django.contrib.auth.backends import RemoteUserBackend
 from django.contrib.auth.middleware import RemoteUserMiddleware
+from django.contrib.auth.models import Group
 
 
 class CustomRemoteUserMiddleware(RemoteUserMiddleware):
+    header = 'HTTP_X_FORWARDED_SSO'
 
     def process_request(self, request):
-        print(f'{self.header}:{request.META.get(self.header)}')
+        if settings.DEBUG:
+            print('META\n', "\n".join(request.META.keys()))
+            print('HEAD\n', "\n".join(request.headers.keys()))
+            print('ENV\n', "\n".join(request.environ.keys()))
+            print(f'{self.header}:{request.META.get(self.header)}')
         super().process_request(request)
 
     def clean_username(self, username, request):
@@ -13,6 +20,7 @@ class CustomRemoteUserMiddleware(RemoteUserMiddleware):
 
 
 class CustomUserBackend(RemoteUserBackend):
+
     def authenticate(self, request, remote_user):
         print(f'Auth username {remote_user}')
         return super().authenticate(request, remote_user)
@@ -22,5 +30,18 @@ class CustomUserBackend(RemoteUserBackend):
         return super().clean_username(username)
 
     def configure_user(self, request, user):
+        user = super().configure_user(request, user)
+        try:
+            user.is_staff = True
+            user.is_active = True
+            try:
+                group = Group.objects.get(name='viewer')
+                user.groups.add(group)
+            except Group.DoesNotExist:
+                pass
+
+            user.save()
+        except Exception:
+            pass
         print(f'Configure username {user}')
-        return super().configure_user(request, user)
+        return user
