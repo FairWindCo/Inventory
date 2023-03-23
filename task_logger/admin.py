4bb9b0bd63b51f8d0c483bdaa759ee2999008868
FairWind <sergey.manenok@gmail.com>
2023-03-23 15:04:30 +0200

    fix self task control

--- task_logger/admin.py
+++ task_logger/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib.admin import register, display
 
+from dictionary.models import ServerScheduledTask
 from task_logger.models import ServerTaskReport, TaskControl, TaskControlGroup
 
 
@@ -28,12 +29,12 @@ class TastReportAdmin(admin.ModelAdmin):
 
 @register(TaskControlGroup)
 class ControlGroupAdmin(admin.ModelAdmin):
-    search_fields = ('name', )
+    search_fields = ('name',)
 
 
 @register(TaskControl)
 class ControlTaskReportAdmin(admin.ModelAdmin):
-    list_display = ('show_name', 'code', 'last_execute')
+    list_display = ('show_name', 'show_code', 'last_execute', 'show_group', 'last_message')
     search_fields = ('host__name', 'code', 'message')
     autocomplete_fields = ('host', 'control_group')
     list_filter = ('host__name', 'code')
@@ -42,3 +43,15 @@ class ControlTaskReportAdmin(admin.ModelAdmin):
     @display(description='Сервер')
     def show_name(self, obj):
         return obj.host.name
+
+    @display(description='Код задачі')
+    def show_code(self, obj):
+        try:
+            task_info = ServerScheduledTask.objects.get(code=obj.code)
+            return f'{obj.code} - {task_info.name}'
+        except ServerScheduledTask.DoesNotExist:
+            return obj.code
+
+    @display(description='Група завдань')
+    def show_group(self, obj):
+        return obj.control_group.name if obj.control_group else '-'
